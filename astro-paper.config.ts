5dae4d2cc021f49e0c606edf443d3beaa8956a4a
Feiran Wang <brianwang2007xjtu@gmail.com>
2026-08-14 22:10:47 +0800

    Update site URL in astro-paper.config.ts

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,7 +2,7 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://brianwang2007xjtu-github-io-qz8u.vercel.app/",
+    url: "https://brianwang2007xjtu-github-io.vercel.app/",
     title: "Master's Blog",
     description: "A minimal, responsive and SEO-friendly Astro blog theme.",
     author: "Sat Naing",
